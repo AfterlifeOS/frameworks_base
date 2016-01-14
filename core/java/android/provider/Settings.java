de2b0e5098579424e317895dc484215a19b6bf52
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    Co-authored-by: xyyx <xyyx@mail.ru>
    Change-Id: I4cca29a49eb9ff6b8353767fdb32a23e94250bf2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6146,6 +6146,13 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
