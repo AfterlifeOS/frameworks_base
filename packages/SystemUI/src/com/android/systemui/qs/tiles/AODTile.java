6c27b1b50d4f2acc961b0217469c105c28a2daa0
maxwen <max.weninger@gmail.com>
2018-06-24 14:05:42 +0200

    AODTile: Add back config check for aod available
    Change-Id: Ibf905de0e0084542e2e4b987ef228db457aa2a6a
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
@@ -67,9 +67,8 @@ public final class AODTile extends QSTileImpl<BooleanState> {
 
     @Override
     public boolean isAvailable() {
-        //final boolean deviceKeys = mContext.getResources().getBoolean(
-        //        com.android.internal.R.bool.config_showNavigationBar);
-        return true;
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_dozeAlwaysOnDisplayAvailable);
     }
 
     @Override
@@ -114,7 +113,7 @@ public final class AODTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.SUPERIOR;
+        return MetricsEvent.AFTERLIFE;
     }
 
     @Override
