53edccde227333e3907faeb6e53c9d0cdeec4335
Jesse Chan <jc@lineageos.org>
2020-05-19 16:05:23 +0800

    SystemUI: Implement hide gestural navigation hint bar
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue May 19 16:05:23 2020 +0800
    
        Implement hide gestural navigation hint bar [1/5]
    
        Change-Id: Ic6695dfe929db443f169d59f153e14a6de249590
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue Jun 2 13:59:52 2020 +0800
    
        SystemUI: Set no hint overlay to highest priority
    
        As overlays are also used to apply navigation mode, it is needed to set
        our customization overlay to highest priority to ensure it is applied.
    
        Change-Id: I41ca4b7ea99fea9f62d2e8ece5114f682e8e7fe9
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun May 15 11:17:03 2022 +0200
    
        SystemUI: Catch IllegalArgumentException in updateHint()
    
        Apparently this can fail for some very unlucky users?
    
        Change-Id: I3609746f9b577b91525fd64f586c2e79663f9ada
    
    Author: alk3pInjection <5e147612@kscope.ink>
    Date:   Thu, 10 Mar 2022 20:25:12 +0800
    
        base: Inverse the navbar hint toggle [1/2]
    
         * Enable -> Disable the gesture bar.
           OEMs have been using such kind of wording for years.
    
        Suggested-by: Jaida Wu <mlgmxyysd@meowcat.org>
        Change-Id: I5f8475c2989fabba59915fdf8ee6613622e2fdeb
    
    Change-Id: If0230bad857e029dc82a79423ef9d3acb03e5421

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12317,6 +12317,12 @@ public final class Settings {
         public static void setLocationProviderEnabled(ContentResolver cr,
                 String provider, boolean enabled) {
         }
+
+        /**
+         * boolean value. toggles swipe up hint in gestural nav mode
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
     }
 
     /**
