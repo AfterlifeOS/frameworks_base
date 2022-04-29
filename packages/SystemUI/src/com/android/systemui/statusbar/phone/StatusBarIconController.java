e585b4b6b9519aa1c3bdd324d110de2acccb5aa9
StarkDroid <gamerprince.exp@gmail.com>
2022-04-29 06:44:36 +0000

    SettingsLib: Design Preference category for corvus
    Change-Id: I9b46c57f9828908a661352169e8c3565d01ba48b
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: priiii0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: Zeref_Igris <igriszeref.002@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -41,6 +41,7 @@ import com.android.internal.statusbar.StatusBarIcon;
 import com.android.systemui.R;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.demomode.DemoModeCommandReceiver;
+import com.android.systemui.Dependency;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.statusbar.BaseStatusBarFrameLayout;
