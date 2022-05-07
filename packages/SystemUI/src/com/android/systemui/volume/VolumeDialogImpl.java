6702f85fd40380122f9e6e76d69cd1719a72abfe
ReallySnow <reallysnow233@gmail.com>
2022-05-07 21:35:52 +0800

    SystemUI: VolumeDialog: New per-app volume icon
    Change-Id: I00e3a3b389a5356499f70822478600b490f144f4
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -324,9 +324,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private int mWindowGravity;
 
-    // Variable to track the default row with which the panel is initially shown
-    private VolumeRow mDefaultRow = null;
-
     private FrameLayout mRoundedBorderBottom;
 
     // Volume panel expand state
