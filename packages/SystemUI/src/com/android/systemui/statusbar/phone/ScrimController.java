6fb4dfce0e22d7adf7bd1715580215ad77002f5d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-12 12:22:06 +0800

    SystemUI: Implement Split notification shade changes [SQUASH]
    Change-Id: Ia21a309da2fbdd48f45c80086303d62b6ce3f77b
    SystemUI: Fix custom scrim transparency for split shade
    
    Change-Id: I22aee216457fcc9cb4fdd66c28d37afea5ebaab3
    
    SystemUI: Hide notification scrim background when entering split shade
    
    Change-Id: I191b9e17d85931f029657abecf9ed648d11e19ae
    [neobuddy89: Adapted for A14 QPR1]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -30,6 +30,8 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.annotation.IntDef;
 import android.app.AlarmManager;
+import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.provider.Settings;
 import android.os.Handler;
@@ -276,6 +278,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     private boolean mWallpaperSupportsAmbientMode;
     private boolean mScreenOn;
     private boolean mTransparentScrimBackground;
+    private boolean mIsLandscape;
 
     // Scrim blanking callbacks
     private Runnable mPendingFrameCallback;
@@ -297,7 +300,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                 mScrimInFront.setViewAlpha(mInFrontAlpha);
 
                 mNotificationsAlpha = alphas.getNotificationsAlpha();
-                mNotificationsScrim.setViewAlpha(mNotificationsAlpha);
+                mNotificationsScrim.setViewAlpha(mIsLandscape ? 0 : mNotificationsAlpha);
+                if (mIsLandscape && mNotificationsScrim != null) {
+                    mNotificationsScrim.setVisibility(View.GONE);
+                }
 
                 mBehindAlpha = alphas.getBehindAlpha();
                 mScrimBehind.setViewAlpha(mBehindAlpha);
@@ -392,6 +398,13 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             public void onUiModeChanged() {
                 ScrimController.this.onThemeChanged();
             }
+
+            @Override
+            public void onConfigChanged(Configuration newConfig) {
+                int orientation = newConfig.orientation;
+                mIsLandscape = orientation == Configuration.ORIENTATION_LANDSCAPE;
+                ScrimController.this.onThemeChanged();
+            }
         });
         mColors = new GradientColors();
         mPrimaryBouncerToGoneTransitionViewModel = primaryBouncerToGoneTransitionViewModel;
@@ -425,6 +438,9 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
         behindScrim.enableBottomEdgeConcave(mClipsQsScrim);
         mNotificationsScrim.enableRoundedCorners(true);
+        if (mIsLandscape && mNotificationsScrim != null) {
+            mNotificationsScrim.setVisibility(View.GONE);
+        }
 
         if (mScrimBehindChangeRunnable != null) {
             mScrimBehind.setChangeRunnable(mScrimBehindChangeRunnable, mMainExecutor);
@@ -971,11 +987,9 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                     mBehindAlpha = mCustomScrimAlpha;
                     mNotificationsAlpha = behindFraction * mCustomScrimAlpha;
                 } else {
-                    mBehindAlpha = mLargeScreenShadeInterpolator.getBehindScrimAlpha(
-                            mPanelExpansionFraction * mDefaultScrimAlpha);
+                    mBehindAlpha = mPanelExpansionFraction * mCustomScrimAlpha;
                     mNotificationsAlpha =
-                            mLargeScreenShadeInterpolator.getNotificationScrimAlpha(
-                                    mPanelExpansionFraction);
+                            mPanelExpansionFraction * mCustomScrimAlpha;
                 }
                 mBehindTint = mState.getBehindTint();
                 mInFrontAlpha = 0;
@@ -1242,7 +1256,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
         setScrimAlpha(mScrimInFront, mInFrontAlpha);
         setScrimAlpha(mScrimBehind, mBehindAlpha);
-        setScrimAlpha(mNotificationsScrim, mNotificationsAlpha);
+        setScrimAlpha(mNotificationsScrim, mIsLandscape ? 0 : mNotificationsAlpha);
 
         // The animation could have all already finished, let's call onFinished just in case
         onFinished(mState);
@@ -1380,7 +1394,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         } else if (scrim == mScrimBehind) {
             return mBehindAlpha;
         } else if (scrim == mNotificationsScrim) {
-            return mNotificationsAlpha;
+            return mIsLandscape? 0 : mNotificationsAlpha;
         } else {
             throw new IllegalArgumentException("Unknown scrim view");
         }
@@ -1453,7 +1467,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             mNotificationsTint = mState.getNotifTint();
             updateScrimColor(mScrimInFront, mInFrontAlpha, mInFrontTint);
             updateScrimColor(mScrimBehind, mBehindAlpha, mBehindTint);
-            updateScrimColor(mNotificationsScrim, mNotificationsAlpha, mNotificationsTint);
+            updateScrimColor(mNotificationsScrim, mIsLandscape ? 0 : mNotificationsAlpha, mNotificationsTint);
         }
     }
 
@@ -1589,10 +1603,12 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             state.setSurfaceColor(surface);
         }
 
-        mBehindColors.setMainColor(surfaceBackground);
+        // hide the surface on landscape 
+        final boolean showSurface = !mIsLandscape;
+        mBehindColors.setMainColor(showSurface ? surfaceBackground : background);
         mBehindColors.setSecondaryColor(accent);
-        mBehindColors.setSupportsDarkText(
-                ColorUtils.calculateContrast(mBehindColors.getMainColor(), Color.WHITE) > 4.5);
+        final boolean isSurfaceBackgroundLight = !ContrastColorUtil.isColorDark(surfaceBackground);
+        mBehindColors.setSupportsDarkText(showSurface ? isSurfaceBackgroundLight : isBackgroundLight);
 
         mNeedsDrawableColorUpdate = true;
     }
