757821dcf3fe3a711d57b87985adf649daa7bff8
NRanjan-17 <nalinishranjan05@gmail.com>
2023-07-15 13:19:22 +0530

    tiles: adapt for AfterLife
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -166,7 +166,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EVO_QS_TILES;
+        return MetricsEvent.AFTERLIFE;
     }
 
     private void startCountDown(long duration) {
