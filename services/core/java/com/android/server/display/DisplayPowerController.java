Neoteric-OS/android_frameworks_base

Animate backlight quickly when HDR mode is changed
Symptom:
During adaptive brightness is ON, brightness animation of HDR layer is
slowly animated when HDR mode is changed.

Root cause:
Slow animation is always applied during adaptive brightness regardless
of HDR mode change.

Solution:
Apply fast animation when HDR mode is changed.

Bug: 221768452
Test: manual - entering/exiting HDR mode and check animation speedy.
Change-Id: I24f8a37cd929144cf38ef72286d72a2ca17daa64

services/core/java/com/android/server/display/DisplayPowerController.java
@@ -337,6 +337,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     private boolean mAppliedTemporaryBrightness;
     private boolean mAppliedTemporaryAutoBrightnessAdjustment;
     private boolean mAppliedBrightnessBoost;
+    private boolean mAppliedHDR;
 
     // Reason for which the brightness was last changed. See {@link BrightnessReason} for more
     // information.
@@ -1365,11 +1366,20 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             // use instead. We still preserve the calculated brightness for Standard Dynamic Range
             // (SDR) layers, but the main brightness value will be the one for HDR.
             float sdrAnimateValue = animateValue;
-            if (mHbmController.getHighBrightnessMode() == BrightnessInfo.HIGH_BRIGHTNESS_MODE_HDR
-                    && ((mBrightnessReason.modifier & BrightnessReason.MODIFIER_DIMMED) == 0
-                    || (mBrightnessReason.modifier & BrightnessReason.MODIFIER_LOW_POWER) == 0)) {
-                // We want to scale HDR brightness level with the SDR level
-                animateValue = mHbmController.getHdrBrightnessValue();
+            if (mHbmController.getHighBrightnessMode() == BrightnessInfo.HIGH_BRIGHTNESS_MODE_HDR) {
+                if ((mBrightnessReason.modifier & BrightnessReason.MODIFIER_DIMMED) == 0
+                        || (mBrightnessReason.modifier & BrightnessReason.MODIFIER_LOW_POWER)
+                        == 0) {
+                    // We want to scale HDR brightness level with the SDR level
+                    animateValue = mHbmController.getHdrBrightnessValue();
+                }
+                if (!mAppliedHDR) {
+                    slowChange = false;
+                }
+                mAppliedHDR = true;
+            } else if (mAppliedHDR) {
+                slowChange = false;
+                mAppliedHDR = false;
             }
 
             final float currentBrightness = mPowerState.getScreenBrightness();
@@ -2278,6 +2288,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         pw.println("  mAppliedLowPower=" + mAppliedLowPower);
         pw.println("  mAppliedScreenBrightnessOverride=" + mAppliedScreenBrightnessOverride);
         pw.println("  mAppliedTemporaryBrightness=" + mAppliedTemporaryBrightness);
+        pw.println("  mAppliedHDR=" + mAppliedHDR);
         pw.println("  mDozing=" + mDozing);
         pw.println("  mSkipRampState=" + skipRampStateToString(mSkipRampState));
         pw.println("  mScreenOnBlockStartRealTime=" + mScreenOnBlockStartRealTime);

AfterlifeOS/frameworks_base

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

services/core/java/com/android/server/display/DisplayPowerController.java
@@ -365,6 +365,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     private boolean mAppliedTemporaryAutoBrightnessAdjustment;
     private boolean mAppliedBrightnessBoost;
     private boolean mAppliedThrottling;
+    private boolean mAppliedHDR;
 
     // Reason for which the brightness was last changed. See {@link BrightnessReason} for more
     // information.
@@ -1702,13 +1703,21 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             float sdrAnimateValue = animateValue;
             // TODO(b/216365040): The decision to prevent HBM for HDR in low power mode should be
             // done in HighBrightnessModeController.
-            if (mHbmController.getHighBrightnessMode() == BrightnessInfo.HIGH_BRIGHTNESS_MODE_HDR
-                    && (mBrightnessReasonTemp.modifier & BrightnessReason.MODIFIER_DIMMED) == 0
-                    && (mBrightnessReasonTemp.modifier & BrightnessReason.MODIFIER_LOW_POWER)
-                    == 0) {
-                // We want to scale HDR brightness level with the SDR level, we also need to restore
-                // SDR brightness immediately when entering dim or low power mode.
-                animateValue = mHbmController.getHdrBrightnessValue();
+            if (mHbmController.getHighBrightnessMode() == BrightnessInfo.HIGH_BRIGHTNESS_MODE_HDR) {
+                if ((mBrightnessReasonTemp.modifier & BrightnessReason.MODIFIER_DIMMED) == 0
+                        || (mBrightnessReasonTemp.modifier & BrightnessReason.MODIFIER_LOW_POWER)
+                        == 0) {
+                    // We want to scale HDR brightness level with the SDR level, we also need to restore
+                    // SDR brightness immediately when entering dim or low power mode.
+                    animateValue = mHbmController.getHdrBrightnessValue();
+                }
+                if (!mAppliedHDR) {
+                    slowChange = false;
+                }
+                mAppliedHDR = true;
+            } else if (mAppliedHDR) {
+                slowChange = false;
+                mAppliedHDR = false;
             }
 
             final float currentBrightness = mPowerState.getScreenBrightness();
@@ -2801,6 +2810,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         pw.println("  mAppliedTemporaryAutoBrightnessAdjustment="
                 + mAppliedTemporaryAutoBrightnessAdjustment);
         pw.println("  mAppliedBrightnessBoost=" + mAppliedBrightnessBoost);
+        pw.println("  mAppliedHDR=" + mAppliedHDR);
         pw.println("  mDozing=" + mDozing);
         pw.println("  mSkipRampState=" + skipRampStateToString(mSkipRampState));
         pw.println("  mScreenOnBlockStartRealTime=" + mScreenOnBlockStartRealTime);