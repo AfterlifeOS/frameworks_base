04c5e379b957adfc7cb5f6710264b2a31e4e98fd
Pranav Vashi <neobuddy89@gmail.com>
2023-10-11 14:21:37 +0000

    QSPanel: Use Tuner API for QS tile animation style
    Change-Id: I6834f8a32f96d1e6214cb906f6c0e4eb1e8eeb6a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ari Lesmana <kingarijk66@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6186,21 +6186,21 @@ public final class Settings {
          *
          * @hide
          */
-        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+        public static final String QS_TILE_ANIMATION_STYLE = "qs_tile_animation_style";
 
         /**
          * Change quick settings tiles animation duration
          *
          * @hide
          */
-        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+        public static final String QS_TILE_ANIMATION_DURATION = "qs_tile_animation_duration";
 
         /**
          * Change quick settings tiles interpolator
          *
          * @hide
          */
-        public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
+        public static final String QS_TILE_ANIMATION_INTERPOLATOR = "qs_tile_animation_interpolator";
 
         /**
          * Whether the torch launch gesture when the screen is off should be enabled.
