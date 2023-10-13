bananadroid/android_frameworks_base

statusbar: allow user to customize max visible notification icons [1/2]

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -98,6 +98,7 @@ public class SystemSettings {
         Settings.System.QQS_LAYOUT,
         Settings.System.LOCK_HIDE_STATUS_BAR,
         Settings.System.NAVIGATION_BAR_IME_SPACE,
-        Settings.System.QS_PANEL_STYLE
+        Settings.System.QS_PANEL_STYLE,
+        Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS,
     };
 }

AfterlifeOS/frameworks_base

Signed-off-by: Ari Lesmana <kingarijk66@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -90,9 +90,10 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.AMBIENT_WAKE_GESTURES,
-	Settings.System.QS_TILE_LABEL_HIDE,
+	    Settings.System.QS_TILE_LABEL_HIDE,
         Settings.System.QS_TILE_VERTICAL_LAYOUT,
         Settings.System.QS_LAYOUT,
         Settings.System.QQS_LAYOUT,
+        Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS,
     };
 }