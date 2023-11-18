09043a4657c54220c744a9d309b6f9fb28607ddb
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-19 00:40:47 +0800

    SystemUI: Implement Island notification feature [SQUASHED]
    * slimmed down version of reticker
    
    Squashed:
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Wed, 22 Nov 2023 18:58:49 +0800
        Subject: [PATCH] Island: Update UI Specs and make layout dynamic
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Wed, 22 Nov 2023 20:15:45 +0800
        Subject: [PATCH] Island: Add quick call answer/reject click functionalities
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Sat, 25 Nov 2023 08:01:10 +0800
        Subject: [PATCH] Island: Implement Dynamic Island effects
    
        * includes notification title handling to prevent redundant notification title headings
        * inspired from IOS' Dynamic Island this makes the island notification to initially show a brief summary of the app notification.
          When the user long click/tap the island notification it will expand and show more info about the app notification.
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Sun, 26 Nov 2023 11:39:46 +0800
        Subject: [PATCH] Island: Improvements and fixes
    
        * add support for dynamic icon loading
        * add text overflow handling
        * fix dynamic island effects graphical issues
        * fix and improve tap/click events
        * improve animation performance
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: someone5678 <someone5678@users.noreply.github.com>
        Date: Fri, 8 Dec 2023 04:23:12 +0900
        Subject: [PATCH] Island: Overrides showIsland in ShadeViewControllerEmptyImpl
    
        * Adapt to A14 QPR1 changes
    
        Change-Id: Id465ceaa0723d90066c4189b25ecbc13e60ebe77
    
    Co-authored-by: Dil3mm4 <dil3mm4.dev@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: I8883665faab14e9117d69cd92c79d38b7b2b19dc
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6341,6 +6341,11 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION = "island_notification";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -55,6 +55,7 @@ import android.annotation.Nullable;
 import android.app.StatusBarManager;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Color;
@@ -230,6 +231,7 @@ import com.android.systemui.statusbar.policy.KeyguardUserSwitcherView;
 import com.android.systemui.statusbar.policy.OnHeadsUpChangedListener;
 import com.android.systemui.statusbar.policy.SplitShadeStateController;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.unfold.SysUIUnfoldComponent;
 import com.android.systemui.util.Compile;
 import com.android.systemui.util.Utils;
@@ -250,6 +252,8 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 
 import kotlinx.coroutines.CoroutineDispatcher;
+
+import com.android.systemui.island.IslandView;
 
 @SysUISingleton
 public final class NotificationPanelViewController implements ShadeSurface, Dumpable {
@@ -286,6 +290,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private static final String COUNTER_PANEL_OPEN = "panel_open";
     public static final String COUNTER_PANEL_OPEN_QS = "panel_open_qs";
     private static final String COUNTER_PANEL_OPEN_PEEK = "panel_open_peek";
+
+    private static final String ISLAND_NOTIFICATION =
+            "system:" + Settings.System.ISLAND_NOTIFICATION;
+
     private static final Rect M_DUMMY_DIRTY_RECT = new Rect(0, 0, 1, 1);
     private static final Rect EMPTY_RECT = new Rect();
     /**
@@ -368,6 +376,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private final QuickSettingsController mQsController;
     private final NaturalScrollingSettingObserver mNaturalScrollingSettingObserver;
     private final TouchHandler mTouchHandler = new TouchHandler();
+    private final TunerService mTunerService;
 
     private long mDownTime;
     private boolean mTouchSlopExceededBeforeDown;
@@ -631,6 +640,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     private boolean mBlockedGesturalNavigation = false;
 
+    private IslandView mNotifIsland;
+    private NotificationStackScrollLayout mNotificationStackScroller;
+    private boolean mUseIslandNotification;
+
     private final Runnable mFlingCollapseRunnable = () -> fling(0, false /* expand */,
             mNextCollapseSpeedUpFactor, false /* expandBecauseOfFalsing */);
     private final Runnable mAnimateKeyguardBottomAreaInvisibleEndRunnable =
@@ -794,6 +807,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             KeyguardFaceAuthInteractor keyguardFaceAuthInteractor,
             SplitShadeStateController splitShadeStateController,
             PowerInteractor powerInteractor,
+	    TunerService tunerService,
             KeyguardClockPositionAlgorithm keyguardClockPositionAlgorithm,
             NaturalScrollingSettingObserver naturalScrollingSettingObserver) {
         keyguardStateController.addCallback(new KeyguardStateController.Callback() {
@@ -901,6 +915,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 mSplitShadeStateController.shouldUseSplitNotificationShade(mResources);
         mView.setWillNotDraw(!DEBUG_DRAWABLE);
         mShadeHeaderController = shadeHeaderController;
+	mTunerService = tunerService;
         mLayoutInflater = layoutInflater;
         mFeatureFlags = featureFlags;
         mAnimateBack = mFeatureFlags.isEnabled(Flags.WM_SHADE_ANIMATE_BACK_GESTURE);
@@ -1116,6 +1131,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!keyguardBottomAreaRefactor()) {
             setKeyguardBottomArea(mView.findViewById(R.id.keyguard_bottom_area));
         }
+
+        mNotificationStackScroller = mView.findViewById(R.id.notification_stack_scroller);
+        mNotifIsland = mView.findViewById(R.id.notification_island);
+        mNotifIsland.setScroller(mNotificationStackScroller);
 
         initBottomArea();
 
@@ -1407,6 +1426,12 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             view = stub.inflate();
         }
         return view;
+    }
+    
+    void updateIslandBackground() {
+        boolean nightMode = (mView.getContext().getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+        mNotifIsland.setIslandBackgroundColorTint(nightMode);
     }
 
     @VisibleForTesting
@@ -3054,6 +3079,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     private void setHeadsUpManager(HeadsUpManager headsUpManager) {
         mHeadsUpManager = headsUpManager;
+        mNotifIsland.setHeadsupManager(headsUpManager);
         mHeadsUpManager.addListener(mOnHeadsUpChangedListener);
         mHeadsUpTouchHelper = new HeadsUpTouchHelper(
                 headsUpManager,
@@ -4408,6 +4434,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         public void onThemeChanged() {
             debugLog("onThemeChanged");
             reInflateViews();
+            updateIslandBackground();
+        }
+
+        @Override
+        public void onUiModeChanged() {
+            if (DEBUG_LOGCAT) Log.d(TAG, "onUiModeChanged");
+            resetViews(true);
+            updateIslandBackground();
         }
 
         @Override
@@ -4599,7 +4633,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         positionClockAndNotifications(true /* forceUpdate */);
     }
 
-    private final class ShadeAttachStateChangeListener implements View.OnAttachStateChangeListener {
+    private final class ShadeAttachStateChangeListener implements View.OnAttachStateChangeListener,
+            TunerService.Tunable {
         @Override
         public void onViewAttachedToWindow(View v) {
             mFragmentService.getFragmentHostManager(mView)
@@ -4611,6 +4646,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE), false,
                     mDoubleTapToSleepObserver);
             mDoubleTapToSleepObserver.onChange(true);
+            mTunerService.addTunable(this, ISLAND_NOTIFICATION);
             // Theme might have changed between inflating this view and attaching it to the
             // window, so
             // force a call to onThemeChanged
@@ -4629,6 +4665,18 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mStatusBarStateController.removeCallback(mStatusBarStateListener);
             mConfigurationController.removeCallback(mConfigurationListener);
             mFalsingManager.removeTapListener(mFalsingTapListener);
+        }
+
+        @Override
+        public void onTuningChanged(String key, String newValue) {
+            switch (key) {
+                case ISLAND_NOTIFICATION:
+                    mUseIslandNotification = TunerService.parseIntegerSwitch(newValue, false);
+                    mNotifIsland.setIslandEnabled(mUseIslandNotification);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -5297,5 +5345,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             return super.performAccessibilityAction(host, action, args);
         }
     }
-}
 
+    @Override
+    public void showIsland(boolean show) {
+        if (!mUseIslandNotification) return;
+        mNotifIsland.showIsland(show, getExpandedFraction());
+    }
+
+    protected void updateIslandVisibility() {
+        mNotifIsland.updateIslandVisibility(getExpandedFraction());
+    }
+}
