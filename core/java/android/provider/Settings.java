8f4b0c3ec93175aaad6b9b52d0690fb5af861d38
SagarMakhar <sagarmakhar@gmail.com>
2024-01-16 14:32:22 +0000

    Allow locking tasks to recents [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6471,6 +6471,11 @@ public final class Settings {
         public static final String FORCE_MOUSE_AS_TOUCH = "force_mouse_as_touch";
 
         /**
+         * @hide Lock Task in Recents
+         */
+        public static final String RECENTS_LOCKED_TASKS = "recents_locked_tasks";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
