2c900f7892f3d13a31adbe871c53d5eb5c12b32d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-06 18:42:44 +0800

    SystemUI: Introduce Depth Wallpaper feature [1/2]
    * like ios wallpaper depth, taken from pixel xpert
    * todo: implement subject segmentation when we found a way to import all gms and ml kit dependencies
      aside from importing aars (note to other source maintainers: i tried and but the dependencies were too many so i dropped subject segmentation impl)
    
    Co-authored-by: Siavash <siavash@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -38,6 +38,7 @@ import static com.android.systemui.statusbar.StatusBarState.SHADE;
 import static com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout.ROWS_ALL;
 
 import android.annotation.Nullable;
+import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
 import android.app.IWallpaperManager;
 import android.app.KeyguardManager;
@@ -89,6 +90,8 @@ import android.view.IWindowManager;
 import android.view.MotionEvent;
 import android.view.ThreadedRenderer;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewConfiguration;
 import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
@@ -238,6 +241,7 @@ import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
 import com.android.systemui.util.WallpaperController;
+import com.android.systemui.util.WallpaperDepthUtils;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.concurrency.MessageRouter;
 import com.android.systemui.util.kotlin.JavaAdapter;
@@ -451,6 +455,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
     private final TunerService mTunerService;
 
     protected GameSpaceManager mGameSpaceManager;
+    
+    private final WallpaperDepthUtils mWallpaperDepthUtils;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -876,6 +882,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         if (predictiveBackSysui()) {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
+        mWallpaperDepthUtils = WallpaperDepthUtils.getInstance(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1104,6 +1111,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
                 (requestTopUi, componentTag) -> mMainExecutor.execute(() ->
                         mNotificationShadeWindowController.setRequestTopUi(
                                 requestTopUi, componentTag))));
+                                
+		ViewGroup rootView = (ViewGroup) getNotificationShadeWindowView().findViewById(R.id.scrim_behind).getParent();
+		@SuppressLint("DiscouragedApi")
+		ViewGroup targetView = rootView.findViewById(mContext.getResources().getIdentifier("notification_container_parent", "id", mContext.getPackageName()));
+		targetView.addView(mWallpaperDepthUtils.getDepthWallpaperView(), 1);
     }
 
     @VisibleForTesting
@@ -2651,6 +2663,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            mWallpaperDepthUtils.updateDepthWallpaperVisibility();
         }
 
         /**
