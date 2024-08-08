4ae6a1d802b896b53f750f0d8d9b3e0adfcb587e
adithya2306 <gh0strider.2k18.reborn@gmail.com>
2024-08-08 13:12:32 +0000

    SystemUI: Implement Bluetooth dialog in QS Based on Internet dialog.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -51,6 +51,7 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.qs.tiles.dialog.BluetoothDialogFactory;
 import com.android.systemui.qs.tiles.dialog.bluetooth.BluetoothTileDialogViewModel;
 import com.android.systemui.res.R;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -68,9 +69,13 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
 
     private static final Intent BLUETOOTH_SETTINGS = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
 
+    private final Handler mHandler;
+
     private static final String TAG = BluetoothTile.class.getSimpleName();
 
     private final BluetoothController mController;
+
+    private final BluetoothDialogFactory mBluetoothDialogFactory;
 
     private CachedBluetoothDevice mMetadataRegisteredDevice = null;
 
@@ -94,11 +99,14 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
             BluetoothController bluetoothController,
             FeatureFlags featureFlags,
             BluetoothTileDialogViewModel dialogViewModel,
-            KeyguardStateController keyguardStateController
+            KeyguardStateController keyguardStateController,
+            BluetoothDialogFactory bluetoothDialogFactory
     ) {
         super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger, keyguardStateController);
+        mHandler = mainHandler;
         mController = bluetoothController;
+        mBluetoothDialogFactory = bluetoothDialogFactory;
         mController.observe(getLifecycle(), mCallback);
         mExecutor = new HandlerExecutor(mainHandler);
         mFeatureFlags = featureFlags;
@@ -107,7 +115,9 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
 
     @Override
     public BooleanState newTileState() {
-        return new BooleanState();
+        BooleanState s = new BooleanState();
+        s.forceExpandIcon = true;
+        return s;
     }
 
     @Override
@@ -119,11 +129,7 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
         if (mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG)) {
             mDialogViewModel.showDialog(mContext, view);
         } else {
-            // Secondary clicks are header clicks, just toggle.
-            final boolean isEnabled = mState.value;
-            // Immediately enter transient enabling state when turning bluetooth on.
-            refreshState(isEnabled ? null : ARG_SHOW_TRANSIENT_ENABLING);
-            mController.setBluetoothEnabled(!isEnabled);
+            mHandler.post(() -> mBluetoothDialogFactory.create(true, view));
         }
     }
 
