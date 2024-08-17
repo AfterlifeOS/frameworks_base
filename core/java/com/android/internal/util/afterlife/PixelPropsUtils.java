f52dde862a8217231cf1ec56fb92e14cc0358401
Hirokixd <86230878+Wrdn28@users.noreply.github.com>
2024-08-17 13:26:18 +0000

    PixelPropUtils: Switch fingerprint to akita
    Signed-off-by: Hirokixd <86230878+Wrdn28@users.noreply.github.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- core/java/com/android/internal/util/afterlife/PixelPropsUtils.java
+++ core/java/com/android/internal/util/afterlife/PixelPropsUtils.java
@@ -57,13 +57,13 @@ public class PixelPropsUtils {
     private static final Map<String, String> DEFAULT_VALUES = Map.of(
         "BRAND", "google",
         "MANUFACTURER", "Google",
-        "DEVICE", "husky",
-        "FINGERPRINT", "google/husky_beta/husky:15/AP31.240617.009/12094726:user/release-keys",
-        "MODEL", "Pixel 8 Pro",
-        "PRODUCT", "husky_beta",
+        "DEVICE", "akita",
+        "FINGERPRINT", "google/akita_beta/akita:15/AP31.240617.015/12207491:user/release-keys",
+        "MODEL", "Pixel 8a",
+        "PRODUCT", "akita_beta",
         "DEVICE_INITIAL_SDK_INT", "21",
-        "SECURITY_PATCH", "2024-07-05",
-        "ID", "AP31.240617.009"
+        "SECURITY_PATCH", "2024-08-05",
+        "ID", "AP31.240617.015"
     );
 
     static {
